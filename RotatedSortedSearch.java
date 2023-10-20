af3167c0d81650da70f29aa59d5a1a67d96b9877
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2023-10-21 01:15:45 +0530

    Add files via upload
    The provided Java code defines a class called `RotatedSortedSearch` with a `search` method for efficiently finding a target element in a rotated and sorted array. The code employs a modified binary search algorithm to handle the rotation of the array. It checks the middle element to determine in which half (L1 or L2) the target element might be. Depending on this determination, it narrows down the search range by recursively calling the `search` method on the appropriate half of the array. This process continues until the target element is found or the search range becomes empty. The code has a time complexity of O(log n) and works well for searching in rotated and sorted arrays, as demonstrated in the `main` method where it searches for the target element `0` in an example array.

---
+++ RotatedSortedSearch.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class RotatedSortedSearch {
+    public static int search(int arr[],int tar,int si,int ei )
+    {
+        if(si>ei)
+        return -1;
+
+        int mid= si+(ei-si)/2;
+
+        if(arr[mid]==tar)
+        return mid;
+
+        //mid on L1
+        if(arr[si]<=arr[mid]){
+            //case a :left
+            if(arr[si]<=tar && tar<=arr[mid])
+            return search(arr, tar, si, mid-1);
+            else //case b:right
+            return search(arr, tar,mid+1, ei);
+        }
+        //mid on L2
+        else{
+            //case c:right
+            if(arr[mid]<=tar && tar<=arr[ei])
+            return search(arr, tar, mid+1, ei);
+            else //case d: left
+            return search(arr, tar, si, mid-1);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={4,5,6,7,0,1,2};
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Searching for 0:"+search(arr, 0, 0, arr.length-1));
+    }
+}
