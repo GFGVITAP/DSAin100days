ba6a6c60951d4a51b5df25bdd570567602b6949c
Himanshu Kumar Sah <114276684+HimanshuKumarSah@users.noreply.github.com>
2023-10-28 00:53:36 +0530

    Create PostfixCalculator.java
    This program implements a simple calculator using a stack to evaluate arithmetic expressions in postfix notation. It can handle addition, subtraction, multiplication, and division.

---
+++ PostfixCalculator.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class PostfixCalculator {
+    public static int evaluatePostfix(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expression.split(" ");
+
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(operand1 + operand2);
+                        break;
+                    case "-":
+                        stack.push(operand1 - operand2);
+                        break;
+                    case "*":
+                        stack.push(operand1 * operand2);
+                        break;
+                    case "/":
+                        stack.push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+}
