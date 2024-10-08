c37dbc86b3d039f3bdfa7e4dbf51d68d1b91e6bd
Parimi Monish <141389332+monish-parimi@users.noreply.github.com>
2024-10-08 23:52:45 +0530

    Insertion Sort in Python

---
+++ DSA in Python/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+array = [5, 4, 3, 2, 1]
+insertion_sort(array)
+print(array)
